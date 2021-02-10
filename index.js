98dfac79bd4975725cd25eec0a00a649f16801dd
David Biggs <david@Davids-MacBook-Pro.local>
2021-02-09 18:34:02 -0600

    Finished JavaScript.

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-// alert("Hello, world!");
-function rot13(str) {
-  const newArr = str.split("");
+const form = document.querySelector("#cc-form");
+const input = document.querySelector("#cc-input");
+const ccResult = document.querySelector("#cc-result");
 
-  return newArr
-    .map((el) => {
-      return isAlphabetic(el) ? shift13(el) : el;
-    })
-    .join("");
+form.addEventListener("submit", checkStr, false);
+input.addEventListener("focus", setFocusDirections, false);
+// input.addEventListener("blur", setBlurDirections, false);
 
-  function shift13(char) {
-    const num = char.charCodeAt();
-    const newNum = num + 13 > 90 ? num + 13 - 90 + 64 : num + 13;
-    return String.fromCharCode(newNum);
+// function executed for its side effects
+function checkStr(event) {
+  const inputStr = event.srcElement[0].value;
+  ccResult.textContent = "Checking...";
+  const res = rot13(inputStr);
+
+  setTimeout(() => {
+    if (!res) {
+      ccResult.textContent = "You should probably type something.";
+    } else {
+      ccResult.textContent = res;
+    }
+  }, 200);
+
+  // pure function, returns converted version of a string.
+  function rot13(str) {
+    const newArr = str.split("");
+
+    return newArr
+      .map((el) => {
+        return isAlphabetic(el) ? shift13(el) : el;
+      })
+      .join("");
+
+    function shift13(char) {
+      const num = char.charCodeAt();
+      const newNum = num + 13 > 90 ? num + 13 - 90 + 64 : num + 13;
+      return String.fromCharCode(newNum);
+    }
+
+    function isAlphabetic(char) {
+      const num = char.charCodeAt();
+      return num >= 65 && num <= 90;
+    }
   }
+}
 
-  function isAlphabetic(char) {
-    const num = char.charCodeAt();
-    return num >= 65 && num <= 90;
-  }
+function setFocusDirections() {
+  ccResult.textContent = "Press enter to evaluate.";
+}
+
+function setBlurDirections() {
+  ccResult.textContent = "Press tab or select the input.";
 }
